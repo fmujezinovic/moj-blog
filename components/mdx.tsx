b2451f618b9c0b81761d4eb4ee91a492fc9a45fe
fmujezinovic <53880402+fmujezinovic@users.noreply.github.com>
2025-06-08 12:30:46 +0200

    vercel 34 mdx

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,17 +1,20 @@
-'use client';
+// components/mdx.tsx
+'use client'
 
-import * as React from 'react';
-import { MDXProvider } from '@mdx-js/react';
-import LottiePlayer from './Lottie';
+import * as React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+import LottiePlayer from './Lottie'
 
 const components = {
   Lottie: LottiePlayer,
-  img: (props) => <img {...props} className="rounded-md shadow-md" />,
-  h2: (props) => <h2 {...props} className="text-2xl text-blue-600" />,
-};
-
-
-export function MDXWrapper({ children }: { children: React.ReactNode }) {
-  return <MDXProvider components={components}>{children}</MDXProvider>;
+  img: (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
+    <img {...props} className="rounded-md shadow-md" />
+  ),
+  h2: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h2 {...props} className="text-2xl text-blue-600" />
+  ),
 }
 
+export function MDXWrapper({ children }: { children: React.ReactNode }) {
+  return <MDXProvider components={components}>{children}</MDXProvider>
+}
