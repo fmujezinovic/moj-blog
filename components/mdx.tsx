5040f4975b0a4e1a9ddde45c4468ca6507aaae75
fmujezinovic <53880402+fmujezinovic@users.noreply.github.com>
2025-06-08 12:34:29 +0200

    vercel 35 mdx

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,12 +1,9 @@
-// components/mdx.tsx
 'use client'
 
 import * as React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import LottiePlayer from './Lottie'
 
 const components = {
-  Lottie: LottiePlayer,
   img: (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
     <img {...props} className="rounded-md shadow-md" />
   ),
