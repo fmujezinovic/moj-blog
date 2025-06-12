cb7a46f583f6e1f844664670dc463a8e8e6269dc
fmujezinovic <53880402+fmujezinovic@users.noreply.github.com>
2025-06-12 15:23:56 +0200

    vercel 56 supabase server

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,14 +2,13 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
 export function createClient() {
-  const cookieStore = cookies();
-
   return createServerClient<any>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
+        async get(name: string) {
+          const cookieStore = await cookies(); // čekamo ovde
           return cookieStore.get(name)?.value;
         },
       },
